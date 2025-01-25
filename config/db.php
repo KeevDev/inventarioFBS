b2fd63aebda86dbb01fd8c627155a5bc88fb666e
Your Name <you@example.com>
2025-01-25 17:52:31 -0500

    sa

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+// require_once 'vendor/autoload.php';
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
