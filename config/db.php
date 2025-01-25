10cb723a7d29d385830434642d0b5de8c840017e
Your Name <you@example.com>
2025-01-25 16:50:10 -0500

    update

--- config/db.php
+++ config/db.php
@@ -1,9 +1,16 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/inventarioFBS/vendor/autoload.php';
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . '/inventarioFBS');
+$dotenv->load();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$host = ''; 
-$db = ''; 
-$user = ''; 
-$pass = ''; 
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME'); 
+$user = getenv('DB_USER'); 
+$pass = getenv('DB_PASS'); 
+
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -13,5 +20,5 @@ if ($conn->connect_error) {
 
 $_SESSION['conn'] = $conn;
 
-// echo "Conexión exitosa!";
+echo "Conexión exitosa!";
 ?>
